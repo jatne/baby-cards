c6be63771579cdfd8abd62958084e808a612051c
Paweł Malok <pawelmalok@gmail.com>
2021-10-02 21:36:56 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,39 @@
-XD
+<script>
+  import { setContext } from 'svelte'
+  import { cards } from './store/cards.js'
+
+  import Slider from './components/Slider.svelte'
+  import Button from './components/Button.svelte'
+
+  setContext('audio', new Audio())
+</script>
+<div class="container">
+  <div class="app-content">
+    <Slider slides={$cards}/>
+  </div>
+  <div class="app-footer">
+    <Button label="Invert colors" />
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    padding: 0 2rem;
+  }
+
+  .app-content {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;;
+  }
+
+  .app-footer {
+    padding: 2rem 0;
+  }
+</style>
---
+++ src/components/Slider.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Card from './Card.svelte'
+  import { Swiper, SwiperSlide } from 'swiper/svelte';
+  import 'swiper/css';
+
+  export let slides = []
+</script>
+
+{#if slides.length}
+  <Swiper
+    loop={true}
+  >
+    {#each slides as slide (slide)}
+      <SwiperSlide>
+        <Card {slide} />
+      </SwiperSlide>
+    {/each}
+  </Swiper>
+{/if}
